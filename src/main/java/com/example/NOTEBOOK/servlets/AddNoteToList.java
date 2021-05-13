544444ea944805b44c967f45da2873f6bc21c494
Echo <aliomaramerica@gmail.com>
2021-05-13 23:26:15 +0600

    createnote page: added some methods

---
+++ src/main/java/com/example/NOTEBOOK/servlets/AddNoteToList.java
@@ -0,0 +1,42 @@
+package com.example.NOTEBOOK.servlets;
+
+import com.example.NOTEBOOK.dao.NotebookDAO;
+import com.example.NOTEBOOK.model.Notebook;
+import com.example.NOTEBOOK.model.User;
+import com.sun.tools.javac.util.JCDiagnostic;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "AddingNote", value = "/AddingNote")
+public class AddNoteToList extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession http = request.getSession();
+        User user = (User) http.getAttribute("user");
+
+        Notebook notebook = new Notebook();
+        NotebookDAO noteDAO = new NotebookDAO();
+
+        notebook.setTitle("");
+        notebook.setText("");
+        notebook.setUser_id(user.getId());
+        String isSuccess = noteDAO.createNote(notebook);
+
+        if(isSuccess.equals("SUCCESS")){
+            user.addNote(notebook);
+            request.getRequestDispatcher("/createNote.jsp").forward(request, response);
+        }else{
+            System.out.println("AddNoteToList.doPost: error");
+            request.getRequestDispatcher("/createNote.jsp").forward(request, response);
+        }
+
+    }
+}
